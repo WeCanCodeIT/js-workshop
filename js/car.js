971e4258ef2e5292a74bc6a0d620ed06ce53921c
Ben Williams <DeanPBR@gmail.com>
2020-06-30 12:59:44 -0400

    Initial commit

---
+++ js/car.js
@@ -0,0 +1,4 @@
+const checkEngine = () => {
+    const checkEngineLight = document.querySelector('.dashboard__check-engine-light');
+    checkEngineLight.classList.toggle("dashboard__check-engine-light--on")
+}
