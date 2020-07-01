6170d2995eee66989544050407f391a97fd1fcf9
Ben Williams <DeanPBR@gmail.com>
2020-07-01 13:56:23 -0400

    Wednesday Push

--- js/car.js
+++ js/car.js
@@ -1,40 +1,47 @@
-const car = {
-    "speed": 0
-}
+class Car {
 
-const accelerate = () => {
-    car.speed += 10;
-}
+    constructor(speedometer, checkEngineLight) {
+        this.checkEngineLight = checkEngineLight;
+        this.speedometer = speedometer;
+        this._speed = 0;
+    }
+    accelerate() {
+        this._speed += 10;
+    }
+    brake() {
+        this._speed -= 10;
+        if (this._speed < 0) {
+            this._speed = 0;
+        }
+    }
+    _checkEngine() {
+        this.checkEngineLight.classList.toggle("dashboard__check-engine-light--on", true);
+    }
+    updateSpeedometer() {
 
-const brake = () => {
-    car.speed -= 10;
-}
-
-const checkEngine = () => {
-    const checkEngineLight = document.querySelector('.dashboard__check-engine-light');
-    checkEngineLight.classList.toggle("dashboard__check-engine-light--on", true)
-}
-
-const updateSpeedometer = () => {
-    const speedometer = document.querySelector(".dashboard__speedometer");
-    speedometer.innerText = car.speed;
-    if (car.speed >= 60) {
-        checkEngine();
+        this.speedometer.innerText = this._speed;
+        if (this._speed >= 60) {
+            this._checkEngine();
+        }
     }
 }
-
-const gasPedal = document.querySelector(".floorboard__gas-pedal");
-
-gasPedal.addEventListener("click", () => {
-    event.preventDefault();
-    accelerate();
-    updateSpeedometer();
-})
-
-const brakePedal = document.querySelector(".floorboard__brake-pedal");
-
-brakePedal.addEventListener("click", () => {
-    event.preventDefault();
-    brake();
-    updateSpeedometer();
-})
+// const car = {
+//     "speed": 0,
+//     accelerate: () => {
+//         car.speed += 10;
+//     },
+//     brake: () => {
+//         car.speed -= 10;
+//     },
+//     checkEngine: () => {
+//         const checkEngineLight = document.querySelector('.dashboard__check-engine-light');
+//         checkEngineLight.classList.toggle("dashboard__check-engine-light--on", true)
+//     },
+//     updateSpeedometer: () => {
+//         const speedometer = document.querySelector(".dashboard__speedometer");
+//         speedometer.innerText = car.speed;
+//         if (car.speed >= 60) {
+//             car.checkEngine();
+//         }
+//     }
+// }
--- js/main.js
+++ js/main.js
@@ -1,7 +1,43 @@
-const titleElement = document.querySelector('#title')
+const speedometer1 = document.querySelector(".car1 .dashboard__speedometer")
+const checkEngineLight1 = document.querySelector(".car1 .dashboard__check-engine-light")
+const car1 = new Car(speedometer1, checkEngineLight1);
+console.log(car1)
+const gasPedal1 = document.querySelector(".car1 .floorboard__gas-pedal");
 
-titleElement.addEventListener("click", (event) => {
-    const eventElement = event.srcElement;
-    eventElement.remove();
-    alert("You clicked the title");
+gasPedal1.addEventListener("click", () => {
+    event.preventDefault();
+    car1.accelerate();
+    car1.updateSpeedometer();
 })
+
+const brakePedal1 = document.querySelector(".car1 .floorboard__brake-pedal");
+
+brakePedal1.addEventListener("click", () => {
+    event.preventDefault();
+    car1.brake();
+    car1.updateSpeedometer();
+})
+const speedometer2 = document.querySelector(".car2 .dashboard__speedometer")
+const checkEngineLight2 = document.querySelector(".car2 .dashboard__check-engine-light")
+const car2 = new Car(speedometer2, checkEngineLight2);
+const gasPedal2 = document.querySelector(".car2 .floorboard__gas-pedal");
+
+gasPedal2.addEventListener("click", () => {
+    event.preventDefault();
+    car2.accelerate();
+    car2.updateSpeedometer();
+})
+
+const brakePedal2 = document.querySelector(".car2 .floorboard__brake-pedal");
+
+const theAButton = 0 //
+theAButton.addEventListener("keyUp", () => {
+
+})
+brakePedal2.addEventListener("click", () => {
+    event.preventDefault();
+    car2.brake();
+    car2.updateSpeedometer();
+})
+
+car2._speed = 300;
